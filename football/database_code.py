25b5b4cc2f513ae8dd25c4bf0a4810ffcb42c4f1
IsThatYouCarl <90243903+IsThatYouCarl@users.noreply.github.com>
2024-05-09 00:29:07 +0800

    Add files via upload

---
+++ football/database_code.py
@@ -0,0 +1,8 @@
+
+DATABASE_CODE = { 
+    2002: "football\\league_standings_database\\bundesliga-league-tables.csv",
+    2015: "football\\league_standings_database\\ligue1-league-tables.csv",
+    2021: "football\\league_standings_database\\premier-league-tables.csv",
+    2014: "football\\league_standings_database\\laliga-league-tables.csv",
+    2019: "football\\league_standings_database\\serieA-league-tables.csv"  
+}
